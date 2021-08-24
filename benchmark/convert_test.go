a896561ad3c6abad94493ee9d7035481d47d5757
Yusuke Hata <yusuke.hata@mirrativ.co.jp>
2021-08-24 14:39:26 +0900

    memory endian order log

--- benchmark/convert_test.go
+++ benchmark/convert_test.go
@@ -26,3 +26,11 @@ func BenchmarkConvert(b *testing.B) {
 		}
 	})
 }
+
+func TestConvertLog(*testing.T) {
+	// little endian
+	println("RGBA", testImg.Pix[0], testImg.Pix[1], testImg.Pix[2], testImg.Pix[3])
+	println("ARGB", testImgARGB.Pix[0], testImgARGB.Pix[1], testImgARGB.Pix[2], testImgARGB.Pix[3])
+	println("ABGR", testImgABGR.Pix[0], testImgABGR.Pix[1], testImgABGR.Pix[2], testImgABGR.Pix[3])
+	println("BGRA", testImgBGRA.Pix[0], testImgBGRA.Pix[1], testImgBGRA.Pix[2], testImgBGRA.Pix[3])
+}
